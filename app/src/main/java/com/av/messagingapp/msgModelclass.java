549dd3be091e0d2171c9f70670c2d9a6b724bf20
skypaul100 <61193125+skypaul100@users.noreply.github.com>
2023-04-25 15:26:00 +0530

    Update msgModelclass.java

--- app/src/main/java/com/av/messagingapp/msgModelclass.java
+++ app/src/main/java/com/av/messagingapp/msgModelclass.java
@@ -1,46 +1,4 @@
-//package com.av.avmessenger;
-//
-//public class msgModelclass {
-//    String message;
-//    String senderid;
-//    long timeStamp;
-//
-//    public msgModelclass() {
-//    }
-//
-//    public msgModelclass(String message, String senderid, long timeStamp) {
-//        this.message = message;
-//        this.senderid = senderid;
-//        this.timeStamp = timeStamp;
-//    }
-//
-//    public String getMessage() {
-//        return message;
-//    }
-//
-//    public void setMessage(String message) {
-//        this.message = message;
-//    }
-//
-//    public String getSenderid() {
-//        return senderid;
-//    }
-//
-//    public void setSenderid(String senderid) {
-//        this.senderid = senderid;
-//    }
-//
-//    public long getTimeStamp() {
-//        return timeStamp;
-//    }
-//
-//    public void setTimeStamp(long timeStamp) {
-//        this.timeStamp = timeStamp;
-//    }
-//}
-
-
-package com.av.avmessenger;
+package com.av.messagingapp;
 
 public class msgModelclass {
     String message;
